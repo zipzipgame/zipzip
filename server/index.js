6e4ac539af813f18b06530d23dad828c6e725d48
Mustafa Camurcu <camurcu.m@husky.neu.edu>
2017-10-15 03:31:01 -0400

    game

--- server/index.js
+++ server/index.js
@@ -11,55 +11,94 @@ wsServer = new WebSocketServer({
 	httpServer: server
 });
 
+var game = {
+	balls: [{
+		x:   0.5,
+		y:   0.2,
+		vy: -0.01,
+		vx:  0.0,
+		r:   0.07,
+	}],
+	players: [],
+};
 
+id = 2;
 
-class Game {
-	constructor(ball) {
-		this.ball = ball;
-		this.players = [];
-	}
-}
+colors = ['blue', 'red', 'purple', 'cyan', 'yellow', 'pink', 'black'];
 
-game = new Game({
-	x:0.5,
-	y:0.2,
-	r:0.0002,
-});
-
-id = 5;
+connections = {};
 
 wsServer.on('request', function(request) {
 	var connection = request.accept(null, request.origin);
-	var playerID = id++;
-	var player = {id:playerID};
+	var playerId = id++;
+	var player = {
+		id: playerId,
+		x: 0.2,
+		y: 0.1,
+		w: 0.0001,
+		color: colors[id%colors.length],
+	};
+	connections[playerId] = connection;
+
 	game.players.push(player);
-	console.log(player);
+	console.log("Player " + playerId + " has connected.");
 
 	let message = {
 		type: "init",
 		playerId: playerId,
+		players: game.players,
 	}
 
 	connection.send(JSON.stringify(message));
 
 	connection.on('message', function(message) {
-		if (message.type === 'init') {
-			
+		let action = JSON.parse(message.utf8Data)['action'];
+		if (action === 'jump') {
+			console.log('jump');
+		} else if (action === 'left') {
+			console.log('left');
+		} else if (action === 'right') {
+			console.log('right');
+		} else if (action === 'stop') {
+			console.log('stop');
 		}
-		connection.send(message.utf8Data));
 	});
 
 	connection.on('close', function(connection) {
-			
+		console.log('Player ' + playerId + ' has disconnected.');
+		delete connections[playerId];
 	});
-
-	setInterval(function(){
-		let message = {
-			type: "update",
-			game: game
-		};
-		connection.send(JSON.stringify(message));
-	}, 3000);
 });
+
+
+let g = -.3; 
+
+let tick = function(dt) {
+	for (let ball of game.balls) {
+		ball.vy += g * dt;
+		ball.x += ball.vx * dt;
+		ball.y += ball.vy * dt;
+		if (ball.vy < 0 && ball.y - ball.r < 0) {
+			ball.vy = -ball.vy;
+		}
+	}
+}
+
+
+setInterval(function(){
+	tick(0.025);
+	let abemal = {};
+	for (let p of game.players) {
+		abemal[p.id] = {x: p.x, y: p.y};
+	}
+	let message = {
+		type: "update",
+		balls: game.balls,
+		players: abemal,
+	};
+	for (let i in connections) {
+		connections[i].send(JSON.stringify(message));
+	}
+}, 25);
 
 
